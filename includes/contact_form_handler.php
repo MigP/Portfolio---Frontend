b5dc82afad3cbe5a3beba20a29df28fe8bd05c56
Miguel Pinto <mig06@hotmail.co.uk>
2022-04-28 11:40:20 +0200

    Update contact_form_handler.php

--- includes/contact_form_handler.php
+++ includes/contact_form_handler.php
@@ -1,13 +1,24 @@
 <?php
 	if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
-        extract($_POST);
+                extract($_POST);
+                $myemail = 'contact@miguelpinto.dx.am';
+                $myhotmail = 'mig06@hotmail.co.uk';
 
-    	$subject="Frontend portfolio - Message";
-		$body=$message;
-		$to="mig06@hotmail.co.uk";
-		$from = $email;
-		$headers = "From:".$from;
+                $email_address = $_POST['email'];
+                $message = $_POST['message'];
+                $name = $_POST['name'];
 
-		mail($to,$subject,$body,$headers);
+		$to = $myhotmail;
+		$email_subject = "Frontend portfolio - Message from " . $name;
+		$email_body = "You have received a new message. " . " Here are the details:\n Name: " . $name . "\n Email: " . $email_address . "\n Message: \n" . $message;
+		
+		$headers  = "MIME-Version: 1.0\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8\r\n";
+		$headers = "From: " . $myemail . "\n";
+		$headers .= "Reply to: " . $email_address . "\n";
+
+		mail($to, $email_subject, $email_body, $headers);
+                
+                echo "success";
 	}
 ?>
